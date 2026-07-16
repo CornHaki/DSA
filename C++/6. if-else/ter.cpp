81d379ec2e64c747426a95f4e8109596e57cf43d
Dimpal Baishya <121637286+CornHaki@users.noreply.github.com>
2026-07-16 19:29:05 +0000

    Add initial implementation of the ternary operator example in C++

---
+++ C++/6. if-else/ter.cpp
@@ -0,0 +1,12 @@
+// Ternary Operator
+// The ternary operator returns a value based on a condition: if the condition is true, it returns the first value; otherwise, it returns the second value.
+
+// It can be used to replace multiple lines of code with a single line, and is often used to replace simple if...else statements:
+// variable = (condition) ? expressionTrue : expressionFalse;
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
