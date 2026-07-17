d197abdb7652b49cda00a27d14f21fd292472121
Dimpal Baishya <121637286+CornHaki@users.noreply.github.com>
2026-07-17 18:33:24 +0000

    Add implementation of foreach loop with example

---
+++ C++/8. Loops/For Loop/foreach.cpp
@@ -0,0 +1,17 @@
+// foreach loop is used to iterate over elements in a collection or array. It simplifies the syntax and improves readability by eliminating the need for explicit indexing.
+
+// Syntax: 
+/*
+ for (datatype variable : collection) 
+    { // code to be executed }
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    cout << "Array Elements: ";
+    for(int element : arr){
+        cout << element << " ";
+    }
+    return 0;
+}
