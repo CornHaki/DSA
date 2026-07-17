96b6f724894552241f817797bb4e6135a6b6a9b8
Dimpal Baishya <121637286+CornHaki@users.noreply.github.com>
2026-07-17 14:00:34 +0000

    Add implementation of number reversal using while loop

---
+++ C++/8. Loops/eg.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // here number is the variable that we want to reverse. We will use a while loop to reverse the number. 
+    // The loop will continue until the number becomes less than or equal to 10. 
+    // In each iteration, we will extract the last digit of the number using the modulus operator (%), and then we will add that digit to the reversed number after multiplying the reversed number by 10. 
+    // Finally, we will divide the original number by 10 to remove the last digit.
+    int number = 12345;
+    int revNumber = 0;
+    while (number > 10){
+        // Extract the last digit of the number.
+        int lastDigit = number % 10;
+        // Add the last digit to the reversed number after multiplying the reversed number by 10.
+        revNumber = revNumber * 10 + lastDigit;
+        // Remove the last digit from the original number.
+        number = number / 10;
+    }
+    cout << "Reversed number: " << revNumber << endl;
+    return 0;
+}
