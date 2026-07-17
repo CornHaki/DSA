417de75e5984b37fb2f45af65f2275a57ac26cef
Dimpal Baishya <121637286+CornHaki@users.noreply.github.com>
2026-07-17 19:23:25 +0000

    Refine comments for clarity in continue statement explanation

--- C++/9. Break_Continue/continue.cpp
+++ C++/9. Break_Continue/continue.cpp
@@ -1,12 +1,12 @@
 // continue statement is used to skip the current iteration of a loop and move to the next iteration.
 // continue statement is commonly used in loops to skip certain elements based on a specific condition.
 // continue statement allows to bypass the remaining code in the current iteration and proceed to the next iteration.
-// continue statement is particularly useful when we want to skip certain iterations of a loop without exiting the loop entirely.
+// continue statement is particularly useful when we want to skip certain iterations without exiting the loop entirely.
 // continue statement can be used in various types of loops, including for, while, and do-while loops. It provides a way to skip iterations based on a condition, improving efficiency and readability of the code.
+
 // Syntax:
 /*
 continue;
-*/
 */
 #include <iostream>
 using namespace std;
