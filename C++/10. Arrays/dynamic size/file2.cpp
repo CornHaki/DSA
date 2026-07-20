ceae20da10e34f1f0a76d6a9c681313f1b5ddf7b
Dimpal Baishya <121637286+CornHaki@users.noreply.github.com>
2026-07-20 18:05:46 +0000

    Add main function to display car names using a for loop

--- C++/10. Arrays/dynamic size/file2.cpp
+++ C++/10. Arrays/dynamic size/file2.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+int main() {
+    vector <string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    cout << "Using for loop:" << endl;
+    for(int i = 0; i < cars.size(); i++){
+        cout << cars[i] << endl;
+    }
+    return 0;
+}
