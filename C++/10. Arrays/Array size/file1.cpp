e1f1e78673691370c286fa247dc2e4d853b23f52
Dimpal Baishya <121637286+CornHaki@users.noreply.github.com>
2026-07-20 19:12:06 +0000

    Fix array size determination in main function and correct usage of size() method

--- C++/10. Arrays/Array size/file1.cpp
+++ C++/10. Arrays/Array size/file1.cpp
@@ -7,3 +7,17 @@
 // sizeof() operator can be used to determine the size of an array at compile time, but it cannot be used to determine the size of a dynamically allocated array or a pointer.
 // .size() method can be used to determine the size of a vector at runtime, as vectors are dynamic arrays that can grow or shrink in size.
 // sizeof() operator is only applicable to arrays with a fixed size known at compile time, and it cannot be used to determine the size of dynamically allocated arrays or pointers, but .size() method can be used to determine the size of a vector at runtime, as vectors are dynamic arrays that can grow or shrink in size, and .size() method can be used to determine the size of a vector at runtime, as vectors are dynamic arrays that can grow or shrink in size, and .size() method can also be used to determine the size of a fixed-size array at runtime, as long as the array is not passed to a function or decayed to a pointer.
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    for(int i = 0; i < arr.size(); i++) { // this is wrong because arr is an array, not a vector, and arrays do not have a size() method. Instead, we can use sizeof(arr) / sizeof(arr[0]) to get the number of elements in the array.
+        cout << arr[i] << " ";
+    }
+
+    int arr1[5] = {1, 2, 3, 4, 5};
+    for(int i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++) { // this is correct because sizeof(arr1) / sizeof(arr1[0]) gives the number of elements in the array, which is 5 in this case.
+        cout << arr1[i] << " ";
+    }
+    return 0;
+}
