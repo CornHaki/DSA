1abe3d37d3d8f3ab8e43cf3fbe7f807f3e287a9b
Dimpal Baishya <121637286+CornHaki@users.noreply.github.com>
2026-07-23 15:48:37 +0000

    add structure file

---
+++ C++/12. Structure/file1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct Student {
+    string name;
+    int age;
+    float gpa;
+};
+
+int main() {
+    Student student1;
+    student1.name = "John Doe";
+    student1.age = 20;
+    student1.gpa = 3.5;
+
+    cout << "Student Name: " << student1.name << endl;
+    cout << "Student Age: " << student1.age << endl;
+    cout << "Student GPA: " << student1.gpa << endl;
+
+    return 0;
+}
